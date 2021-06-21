b9778a141667ed9c7313a1199b8e26ff8dbed6c4
Kariuki George <85384821+smiley-geek@users.noreply.github.com>
2021-06-21 10:44:20 +0300

    Add files via upload
    changes

--- index.js
+++ index.js
@@ -13,3 +13,58 @@ navButton.addEventListener('click', () => {
     }
     
 });
+
+
+const backthisproject = document.getElementById('backthisproject')
+const backthisprojectpopup = document.getElementById('backthisprojectpop')
+const closepopup = document.getElementById('closepop')
+const thanks = document.getElementById('thanks')
+const input = document.getElementsByTagName('input')
+backthisproject.addEventListener('click', () => {
+    backthisprojectpopup.style.display = 'flex'
+})
+closepopup.addEventListener('click', () => {
+    backthisprojectpopup.style.display = 'none'
+})
+
+const pledgeamount = document.getElementsByClassName('pledgeamount')
+
+const pledges = document.getElementsByClassName('sub-cont')
+
+pledges[6].style.opacity = '0.5'
+pledges[2].style.opacity = '0.5'
+pledges[3].addEventListener('click', () => {
+    
+    thanks.style.display = 'flex'
+    pledges[3].style.border = '1px blue solid'
+    
+})
+pledges[4].addEventListener('click', () => {
+    
+
+    
+    pledges[4].style.border = '1px blue solid'
+    pledgeamount[0].style.display = 'flex'
+    
+})
+pledges[5].addEventListener('click', () => {
+    
+    pledges[5].style.border = '1px blue solid'
+    pledgeamount[1].style.display = 'flex'
+})
+
+
+
+thanks.addEventListener('click', () => {
+    
+    thanks.style.display = 'none'
+    
+})
+
+
+const pop = document.getElementById("pop")
+
+pop.addEventListener('click', () => {
+    thanks.style.display = 'flex'
+    
+})
